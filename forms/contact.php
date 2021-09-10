8a88808ce502db6ace4717a0c3737932848a525d
TBJ <44341859+TBJr@users.noreply.github.com>
2021-09-10 12:28:59 +0000

    contact form updated

--- forms/contact.php
+++ forms/contact.php
@@ -1,6 +1,6 @@
 <?php
  
-  $receiving_email_address = 'tjaybrowne@gmail.com';
+  $receiving_email_address = 'backendbackbonner@outlook.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -17,15 +17,15 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
+
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
     'username' => 'sims.wfp@gmail.com',
     'password' => '!Lov3MyPiano93',
-    // 'port' => '587'
-    'port' => '465'
+    'port' => '587'
+    // 'port' => '465'
   );
-  
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
@@ -34,6 +34,13 @@
   // uncomment below code if you want to send cc and bcc
   // $contact->cc = array('ccreceiver1@example.com', 'ccreceiver2@example.com');
   // $contact->bcc = array('bccreceiver1@example.com', 'bccreceiver2@example.com');
+
+  $contact->invalid_to_email = 'Email to (receiving email address) is empty or invalid!';
+  $contact->invalid_from_name = 'From Name is empty!';
+  $contact->invalid_from_email = 'Email from: is empty or invalid!';
+  $contact->invalid_subject = 'Subject is too short or empty!';
+  $contact->short = 'is too short or empty!'; // If the length check number is set and the provided message text is under the set length in the add_message() method call
+  $contact->ajax_error = 'Sorry, the request should be an Ajax POST'; // If ajax property is set true and the post method is not an AJAX call
 
   echo $contact->send();
 ?>
